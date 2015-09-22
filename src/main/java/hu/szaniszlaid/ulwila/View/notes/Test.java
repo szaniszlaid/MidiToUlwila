d7aa318fcfd44561c96298b31c2a843015fc5d2e
szaniszlaid <david.szaniszlai@adwareresearch.com>
2015-09-22 16:14:55 +0200

    native LookAndFeel resolved #3

--- src/main/java/hu/szaniszlaid/ulwila/View/notes/Test.java
+++ src/main/java/hu/szaniszlaid/ulwila/View/notes/Test.java
@@ -5,61 +5,70 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
 
 import hu.szaniszlaid.ulwila.note.util.Octave;
 
 public class Test extends JFrame {
 
-	private JPanel contentPane;
-	
+    private JPanel contentPane;
+
     private void initUI() {
 
         //add(new TestJPanel());
-        add(new HalfRestPanel());
-        add(new EightRestPanel());
-        add(new SixteenthRestPanel());
-        add(new HalfNotePanel(Color.YELLOW, Octave.FIRST));
-        add(new HalfNotePanel(Color.YELLOW, Octave.SECOND));
-        add(new HalfNotePanel(Color.YELLOW, Octave.THIRD));
-        add(new EighthNotePanel(Color.RED, Octave.FIRST));
-        add(new EighthNotePanel(Color.RED, Octave.SECOND));
-        add(new EighthNotePanel(Color.RED, Octave.THIRD));
-        add(new WholeRestPanel());                
-        add(new QuarterNotePanel(Color.BLUE, Octave.FIRST));
-        add(new QuarterNotePanel(Color.BLUE, Octave.SECOND));
-        add(new QuarterNotePanel(Color.BLUE, Octave.THIRD));
-        add(new SixteenthNotePanel(Color.GREEN, Octave.FIRST));
-        add(new SixteenthNotePanel(Color.GREEN, Octave.SECOND));
-        add(new SixteenthNotePanel(Color.GREEN, Octave.THIRD));
-        add(new WholeNotePanel(Color.MAGENTA, Octave.SECOND));
-
+        getContentPane().add(new HalfRestPanel());
+        getContentPane().add(new EightRestPanel());
+        getContentPane().add(new SixteenthRestPanel());
+        getContentPane().add(new HalfNotePanel(Color.YELLOW, Octave.FIRST));
+        getContentPane().add(new HalfNotePanel(Color.YELLOW, Octave.SECOND));
+        getContentPane().add(new HalfNotePanel(Color.YELLOW, Octave.THIRD));
+        getContentPane().add(new EighthNotePanel(Color.RED, Octave.FIRST));
+        getContentPane().add(new EighthNotePanel(Color.RED, Octave.SECOND));
+        getContentPane().add(new EighthNotePanel(Color.RED, Octave.THIRD));
+        getContentPane().add(new WholeRestPanel());
+        getContentPane().add(new QuarterNotePanel(Color.BLUE, Octave.FIRST));
+        getContentPane().add(new QuarterNotePanel(Color.BLUE, Octave.SECOND));
+        getContentPane().add(new QuarterNotePanel(Color.BLUE, Octave.THIRD));
+        getContentPane().add(new SixteenthNotePanel(Color.GREEN, Octave.FIRST));
+        getContentPane().add(new SixteenthNotePanel(Color.GREEN, Octave.SECOND));
+        getContentPane().add(new SixteenthNotePanel(Color.GREEN, Octave.THIRD));
+        getContentPane().add(new WholeNotePanel(Color.MAGENTA, Octave.SECOND));
 
         setLocationRelativeTo(null);
     }
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Test frame = new Test();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Test frame = new Test();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	public Test() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(500, 500, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
-		setContentPane(contentPane);
-		
-		initUI();
-	}
-	
+    public Test() {
+        //set LookAndFeel to system default
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(500, 500, 524, 411);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+        setContentPane(contentPane);
+
+        initUI();
+    }
+
 }
