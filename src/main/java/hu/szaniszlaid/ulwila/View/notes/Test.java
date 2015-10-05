dff22f155576b8410f91225a090983174f0667b1
szaniszlaid <david.szaniszlai@adwareresearch.com>
2015-10-05 13:38:08 +0200

    Modify layoutManager to BorderLayout with JScrollPane #4 resolved

--- src/main/java/hu/szaniszlaid/ulwila/View/notes/Test.java
+++ src/main/java/hu/szaniszlaid/ulwila/View/notes/Test.java
@@ -1,42 +1,65 @@
 package hu.szaniszlaid.ulwila.View.notes;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.border.EmptyBorder;
 
 import hu.szaniszlaid.ulwila.note.util.Octave;
 
 public class Test extends JFrame {
 
-    private JPanel contentPane;
 
-    private void initUI() {
+    private void initComponents() {
+        
+        //Set sizes of root frame
+        setBounds(500, 500, 800, 500);
+        
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout(0, 0));
+        
+        //TODO Temporary, just for design test
+        JPanel menu = new JPanel();
+        mainPanel.add(menu, BorderLayout.NORTH);
+        JButton btnHha = new JButton("Uzsgyi");
+        menu.add(btnHha);
+                
+        //Test notes
+        JPanel notesPanel = new JPanel();
+        notesPanel.add(new HalfRestPanel());
+        notesPanel.add(new EightRestPanel());
+        notesPanel.add(new SixteenthRestPanel());
+        notesPanel.add(new HalfNotePanel(Color.YELLOW, Octave.FIRST));
+        notesPanel.add(new HalfNotePanel(Color.YELLOW, Octave.SECOND));
+        notesPanel.add(new HalfNotePanel(Color.YELLOW, Octave.THIRD));
+        notesPanel.add(new EighthNotePanel(Color.RED, Octave.FIRST));
+        notesPanel.add(new EighthNotePanel(Color.RED, Octave.SECOND));
+        notesPanel.add(new EighthNotePanel(Color.RED, Octave.THIRD));
+        notesPanel.add(new WholeRestPanel());
+        notesPanel.add(new QuarterNotePanel(Color.BLUE, Octave.FIRST));
+        notesPanel.add(new QuarterNotePanel(Color.BLUE, Octave.SECOND));
+        notesPanel.add(new QuarterNotePanel(Color.BLUE, Octave.THIRD));
+        notesPanel.add(new SixteenthNotePanel(Color.GREEN, Octave.FIRST));
+        notesPanel.add(new SixteenthNotePanel(Color.GREEN, Octave.SECOND));
+        notesPanel.add(new SixteenthNotePanel(Color.GREEN, Octave.THIRD));
+        notesPanel.add(new WholeNotePanel(Color.MAGENTA, Octave.SECOND));
+        
+        notesPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        notesPanel.setPreferredSize(new Dimension(500, 500));
 
-        //add(new TestJPanel());
-        getContentPane().add(new HalfRestPanel());
-        getContentPane().add(new EightRestPanel());
-        getContentPane().add(new SixteenthRestPanel());
-        getContentPane().add(new HalfNotePanel(Color.YELLOW, Octave.FIRST));
-        getContentPane().add(new HalfNotePanel(Color.YELLOW, Octave.SECOND));
-        getContentPane().add(new HalfNotePanel(Color.YELLOW, Octave.THIRD));
-        getContentPane().add(new EighthNotePanel(Color.RED, Octave.FIRST));
-        getContentPane().add(new EighthNotePanel(Color.RED, Octave.SECOND));
-        getContentPane().add(new EighthNotePanel(Color.RED, Octave.THIRD));
-        getContentPane().add(new WholeRestPanel());
-        getContentPane().add(new QuarterNotePanel(Color.BLUE, Octave.FIRST));
-        getContentPane().add(new QuarterNotePanel(Color.BLUE, Octave.SECOND));
-        getContentPane().add(new QuarterNotePanel(Color.BLUE, Octave.THIRD));
-        getContentPane().add(new SixteenthNotePanel(Color.GREEN, Octave.FIRST));
-        getContentPane().add(new SixteenthNotePanel(Color.GREEN, Octave.SECOND));
-        getContentPane().add(new SixteenthNotePanel(Color.GREEN, Octave.THIRD));
-        getContentPane().add(new WholeNotePanel(Color.MAGENTA, Octave.SECOND));
-
-        setLocationRelativeTo(null);
+       
+        JScrollPane scrollPanel = new JScrollPane(notesPanel);
+        mainPanel.add(scrollPanel, BorderLayout.CENTER);
+        
+        setContentPane(mainPanel);
     }
 
     public static void main(String[] args) {
@@ -62,13 +85,9 @@ public class Test extends JFrame {
         }
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(500, 500, 524, 411);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
-        setContentPane(contentPane);
+       
+        initComponents();
 
-        initUI();
     }
 
 }
